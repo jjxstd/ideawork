b485e2f1cb5962a6dfcb7a5b4ac3ffc1d1c03073
jjxstd <jyk18339281537@163.com>
2019-11-25 17:41:22 +0800

    dome-git

---
+++ src/main/java/com/example/demogit/test/Test.java
@@ -0,0 +1,10 @@
+package com.example.demogit.test;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        System.out.println("hello");
+    }
+
+}
